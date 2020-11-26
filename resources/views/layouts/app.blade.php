3fc96977984e9450bba2f7ef95686c38b195890c
Shark-vil <shark_vil@mail.ru>
2020-11-26 17:47:40 +0300

    Добавлены пункты в шаблонах страниц, изменены роуты
    - Добавлена ссылка на домашнюю страницу в разделе wiki
    - Добавлена ссылка на wiki в разделе домашней страницы
    - Добавлен редирект на wiki страницу при заходе в корень сайта

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,10 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('wiki') }}">Wiki</a>
+                        </li>
+
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('wiki');
 });
 
 $AUTH_REGISTER = env('AUTH_REGISTER');
